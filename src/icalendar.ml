ee20e286af40497d45a2d7d8c52b9aa04ac9f014
linse <sschirme@gmail.com>
2018-04-23 17:38:43 +0200

    Add more tests, including a test for quoted strings in properties.

--- src/icalendar.ml
+++ src/icalendar.ml
@@ -8,10 +8,10 @@ let name = take_while1 is_alpha_digit_minus
 let param_name = name
 let is_control = function '\x00' .. '\x08' | '\x0a' .. '\x1f' | '\x7f' -> true | _ -> false
 let is_qsafe_char = function x when is_control x -> false | '"' -> false | _ -> true
-let quoted_string = char '"' *> take_while is_qsafe_char <* char '"'
+let quoted_string = char '"' *> take_while1 is_qsafe_char <* char '"'
 let is_safe_char = function x when is_control x -> false | '"' | ';' | ':' | ',' -> false | _ -> true
-let param_text = take_while is_safe_char
-let param_value = param_text <|> quoted_string
+let param_text = take_while1 is_safe_char
+let param_value = param_text <|> quoted_string (* in contrast to rfc we require at least 1 char for param_value *)
 let param = lift2 collect_param param_name (char '=' *> param_value)
 
 let value = take_while (fun x -> not (is_control x)) (*in fact it is more complicated*)
--- test/test.ml
+++ test/test.ml
@@ -7,13 +7,13 @@ let compare_t =
       && String.compare c c' = 0  
   end in (module M: Alcotest.TESTABLE with type t = M.t)
 
-
+let result = Alcotest.(result (list compare_t) string)
 
 let test_line () = 
   let line = "DESCRIPTION:This is a long description that exists on a long line.\n" in
   let expected = Ok [("DESCRIPTION", [], "This is a long description that exists on a long line.")] in
   let f = Icalendar.parse line in
-  Alcotest.(check (result (list compare_t) string) "test short line" expected f)
+  Alcotest.check result "test short line" expected f
 
 let test_multiline () = 
   let multiline = {__|DESCRIPTION:This is a lo
@@ -22,12 +22,46 @@ let test_multiline () =
 |__} in
   let expected = Ok [("DESCRIPTION", [], "This is a long description that exists on a long line.")] in
   let f = Icalendar.parse multiline in
-  Alcotest.(check (result (list compare_t) string) "test short line" expected f)
+  Alcotest.check result "test short line" expected f
 
+let line_with_parameters () =
+  let line = "ATTENDEE;RSVP=TRUE;ROLE=REQ-PARTICIPANT:mailto:jsmith@example.com\n"
+  and expected = Ok [("ATTENDEE", [("RSVP", "TRUE") ; ("ROLE", "REQ-PARTICIPANT")], "mailto:jsmith@example.com")] in
+  let f = Icalendar.parse line in
+  Alcotest.check result __LOC__ expected f
+
+let line_with_parameter () =
+  let line = "RDATE;VALUE=DATE:19970304,19970504,19970704,19970904\n"
+  and expected = Ok [("RDATE", [("VALUE","DATE")], "19970304,19970504,19970704,19970904")] in
+  let f = Icalendar.parse line in
+  Alcotest.check result __LOC__ expected f
+
+let attach () =
+  let line = "ATTACH:http://example.com/public/quarterly-report.doc\n"
+  and expected = Ok [("ATTACH", [], "http://example.com/public/quarterly-report.doc")] in
+  let f = Icalendar.parse line in
+  Alcotest.check result __LOC__ expected f
+
+let attach_inline_binary () =
+  let line = "ATTACH;FMTTYPE=text/plain;ENCODING=BASE64;VALUE=BINARY:VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZy4\n"
+  and expected = Ok [("ATTACH",[("FMTTYPE","text/plain");("ENCODING","BASE64");("VALUE","BINARY")],"VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZy4")] in
+  let f = Icalendar.parse line in
+  Alcotest.check result __LOC__ expected f
+
+let quoted_prop () =
+  let line = "DESCRIPTION;ALTREP=\"cid:part1.0001@example.org\":The Fall'98 Wild Wizards Conference - - Las Vegas\\, NV\\, USA\n"
+  and expected = Ok [("DESCRIPTION",[("ALTREP","cid:part1.0001@example.org")],"The Fall'98 Wild Wizards Conference - - Las Vegas\\, NV\\, USA")] in
+  let f = Icalendar.parse line in
+  Alcotest.check result __LOC__ expected f
 
 let line_tests = [
   "Parse line", `Quick, test_line ;
-  "Parse multiline", `Quick, test_multiline
+  "Parse multiline", `Quick, test_multiline ;
+  "Parse parameters", `Quick, line_with_parameters ;
+  "Parse parameter", `Quick, line_with_parameter ;
+  "Parse attachment", `Quick, attach ;
+  "Parse inline attachment", `Quick, attach_inline_binary ;
+  "Parse quoted prop", `Quick, quoted_prop ;
 ]
 
 let tests = [ 
